8b248237496f15c4ccf4fdbc259f14b157e5a39b
zhangchao042 <zhangchao042@ke.com>
2019-01-23 18:25:33 +0800

    finish file util

--- app/src/main/java/zc/neu/com/securitystorage/FileUtil.java
+++ app/src/main/java/zc/neu/com/securitystorage/FileUtil.java
@@ -1,9 +1,11 @@
 package zc.neu.com.securitystorage;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -12,6 +14,9 @@ import java.io.IOException;
  */
 public class FileUtil {
 
+  public static final String TXT = "txt_dir";//文本文件夹
+  public static final String IMG = "img_dir";//图片文件夹
+
   /**
    * 文件夹初始化操作
    * txt_dir: 存放文本的文件夹
@@ -19,8 +24,8 @@ public class FileUtil {
    * @param context
    */
   public static void initFile(Context context) throws Exception {
-    File textDir = new File(context.getFilesDir(),"txt_dir");
-    File imgDir = new File(context.getFilesDir(),"img_dir");
+    File textDir = new File(context.getFilesDir(),TXT);
+    File imgDir = new File(context.getFilesDir(),IMG);
     boolean flag_text = true;
     boolean flag_img = true;
     if(!textDir.exists()){
@@ -42,7 +47,7 @@ public class FileUtil {
    * @throws Exception
    */
   public static String getTextFile(Context context, String textName) throws Exception {
-    File textDir = new File(context.getFilesDir(),"txt_dir");
+    File textDir = new File(context.getFilesDir(),TXT);
     File text = new File(textDir,textName);
     if(text.exists()){
       FileInputStream fis = new FileInputStream(text);
@@ -57,15 +62,15 @@ public class FileUtil {
   }
 
   /**
-   *
+   * 保存内容到文件中
    * @param context
-   * @param content
-   * @param textName
+   * @param content 需要保存的内容
+   * @param textName 保存后的文件名
    * @throws IOException
    */
   public static void saveTextFile(Context context, String content, String textName)
       throws IOException {
-    File textDir = new File(context.getFilesDir(),"txt_dir");
+    File textDir = new File(context.getFilesDir(),TXT);
     File text = new File(textDir,textName);
     if(text.exists()){
       text.delete();
@@ -74,6 +79,57 @@ public class FileUtil {
     FileOutputStream fos = new FileOutputStream(text);
     fos.write(content.getBytes("US-ASCII"));
     fos.close();
+  }
+
+  /**
+   * 通过图片名称获取bitmap
+   * 暂时没有添加加密过程，之后会在这里加入
+   * @param context
+   * @param imgName
+   * @return Bitmap
+   * @throws Exception
+   */
+  public static Bitmap getImage(Context context, String imgName) throws Exception {
+    File imgDir = new File(context.getFilesDir(),IMG);
+    File img = new File(imgDir,imgName);
+    if(img.exists()){
+      FileInputStream fis= new FileInputStream(img);
+      int length = fis.available();
+      byte[] buffer = new byte[length];
+      fis.read(buffer);
+      fis.close();
+      return BitmapFactory.decodeByteArray(buffer,0,buffer.length);
+    }else{
+      throw new Exception("image is not exist!");
+    }
+  }
+
+  /**
+   * 保存图片，未进行加解密的操作。
+   * @param context
+   * @param bitmap
+   * @param imageName
+   * @throws IOException
+   */
+  public static void saveImage(Context context, Bitmap bitmap, String imageName)
+      throws IOException {
+    File imgDir = new File(context.getFilesDir(),IMG);
+    File img = new File(imgDir,imageName);
+
+    img.createNewFile();
+    FileOutputStream fos = new FileOutputStream(img);
+    byte[] buffer = Bitmap2Bytes(bitmap);
+    fos.write(buffer);
+    fos.close();
+  }
+
+  /**
+   * 把Bitmap转Byte
+   */
+  private static byte[] Bitmap2Bytes(Bitmap bm){
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    bm.compress(Bitmap.CompressFormat.PNG, 100, baos);
+    return baos.toByteArray();
   }
 
 }
